03dd8f97fbb5cc4454169c9af8e94ebe5bb1f141
Rick Kotlarz <43300572+RickKotlarz@users.noreply.github.com>
2025-04-11 11:48:41 -0400

    Update Create-SCUs-cmdlet.ps1

--- SCU_automation/Create_SCUs_cmdlet/Create-SCUs-cmdlet.ps1
+++ SCU_automation/Create_SCUs_cmdlet/Create-SCUs-cmdlet.ps1
@@ -7,5 +7,6 @@ $overageAmount = 2          ### not needed if overage state is unlimited
 $geoLocation = "eastus"     ### "westeurope"
 $geo = "US"                 ### "EU"
 $crossGeo = "Allowed"       ### "NotAllowed"
+#apiVersion = "2024-11-01-preview"
 
 New-AzResource -ResourceName $resourceName -ResourceType $resourceType  -ResourceGroupName $resourceGroupName -Location $geoLocation -Properties @{numberOfUnits=$numberOfSCUs; overageState=$overageState; overageAmount=$overageAmount; crossGeoCompute=$crossGeo; geo=$geo}
