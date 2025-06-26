6a8f8d85d33f28d1d5dfd74d0ab52afb69783bca
Rick Kotlarz <43300572+RickKotlarz@users.noreply.github.com>
2025-06-26 12:26:54 -0400

    Update Create-SCUs.ps1

--- SCU_automation/Create_SCUs/Create-SCUs.ps1
+++ SCU_automation/Create_SCUs/Create-SCUs.ps1
@@ -1,4 +1,5 @@
-$resourceName = "SCU-created-from-Azure-shell"
+################# Modify as needed #########################
+$resourceName = "SCU-created-from-Azure-shell-v4"
 $resourceGroupName = "SCU-demo-RG"
 $resourceType = "microsoft.securitycopilot/capacities"
 $numberOfSCUs = 1           ### baseline number of SCUs
@@ -8,5 +9,22 @@ $geoLocation = "eastus"     ### "westeurope"
 $geo = "US"                 ### "EU"
 $crossGeo = "Allowed"       ### "NotAllowed"
 $apiVersion = "2024-11-01-preview"
+$tags = @{
+    Department = 'SOC'
+    BillingCode = '12345'
+}
 
-New-AzResource -ResourceName $resourceName -ResourceType $resourceType  -ResourceGroupName $resourceGroupName -Location $geoLocation -Properties @{numberOfUnits=$numberOfSCUs; overageState=$overageState; overageAmount=$overageAmount; crossGeoCompute=$crossGeo; geo=$geo} -ApiVersion $apiVersion
+######### Execute once avove has been editied ##############
+New-AzResource -ResourceName $resourceName `
+    -ResourceType $resourceType `
+    -ResourceGroupName $resourceGroupName `
+    -Location $geoLocation `
+    -Properties @{
+        numberOfUnits = $numberOfSCUs
+        overageState = $overageState
+        overageAmount = $overageAmount
+        crossGeoCompute = $crossGeo
+        geo = $geo
+    } `
+    -ApiVersion $apiVersion `
+    -Tag $tags
